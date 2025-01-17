50f5dad914f05cb400c46f73e9f4dbbde29fd45f
PadillaProjects <padilla.projects27@gmail.com>
2025-01-17 16:40:19 -0700

    Add CNAME file for custom domain

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import ItemDetails from "./pages/ItemDetails";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
-      {/* Header is displayed on all pages */}
       <Header />
-
-      {/* Define routes */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/item/:id" element={<ItemDetails />} />
+        {/* Add other routes here if needed */}
       </Routes>
     </Router>
   );
